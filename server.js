961ccca8eec709a9a3c961ebb92d0aec8132d7d7
simmy bajaj <simmybajaj1603@gmail.com>
2022-03-06 12:07:04 +0530

    changes for initial repository

--- server.js
+++ server.js
@@ -5,17 +5,31 @@ const passport = require("passport");
 const { handleNotFound } = require("./middlewear/error");
 const cors = require("cors");
 
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Max-Age", "1800");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Accept,Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader(
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
     "Access-Control-Allow-Headers",
     "Content-Type, Accept,Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
   );
   next();
 });
