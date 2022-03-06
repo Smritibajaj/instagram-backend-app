200e369f7fbead9a99e90205ca366542da90a800
simmy bajaj <simmybajaj1603@gmail.com>
2022-03-06 12:10:06 +0530

    changes for initial repository

--- server.js
+++ server.js
@@ -5,34 +5,34 @@ const passport = require("passport");
 const { handleNotFound } = require("./middlewear/error");
 const cors = require("cors");
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader("Access-Control-Max-Age", "1800");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Accept,Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
-
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "'https://instagram-clone1-app.herokuapp.com");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Content-Type, Accept,Authorization"
   );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Accept,Authorization"
+//   );
+//   next();
+// });
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
